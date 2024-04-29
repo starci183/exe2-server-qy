4eecd9a2c1fda979185cc35a2ac973d17d8d2d2a
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2024-04-29 11:27:50 +0700

    123

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Exe2')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/', app, document, {
+    swaggerOptions: { defaultModelsExpandDepth: -1 },
+  });
+
+  await app.listen(2310);
+}
+bootstrap();
